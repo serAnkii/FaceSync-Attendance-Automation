999b987b4aba864e41a8e6c6e99cfc17296803ff
Ankit Negi <89020930+serAnkii@users.noreply.github.com>
2023-10-01 20:24:19 +0530

    Update streallit.py

--- streallit.py
+++ streallit.py
@@ -1,15 +1,15 @@
-# import streamlit as st
+import streamlit as st
 
-# import pandas as pd
+import pandas as pd
 
-# import time
+import time
 
-# from datetime import datetime
+from datetime import datetime
 
-# timeobj = time.time()
-# date = datetime.fromtimestamp(timeobj).strftime('%d-%m-%Y')
+timeobj = time.time()
+date = datetime.fromtimestamp(timeobj).strftime('%d-%m-%Y')
 
-# dataframe = pd.read_csv("attendence/attendence_ +" + str(date) +'.csv')
+dataframe = pd.read_csv("attendence/attendence_ +" + str(date) +'.csv')
 
-# st.dataframe(dataframe.style.highlight_max(axis=0))
+st.dataframe(dataframe.style.highlight_max(axis=0))
 
