844418a2b701569f5af51fef8a404a4f8f691790
serAnkii <recruitankitnegi2023@gmail.com>
2023-10-01 15:27:17 +0530

    feat:uploading data to csv

---
+++ streallit.py
@@ -0,0 +1,15 @@
+# import streamlit as st
+
+# import pandas as pd
+
+# import time
+
+# from datetime import datetime
+
+# timeobj = time.time()
+# date = datetime.fromtimestamp(timeobj).strftime('%d-%m-%Y')
+
+# dataframe = pd.read_csv("attendence/attendence_ +" + str(date) +'.csv')
+
+# st.dataframe(dataframe.style.highlight_max(axis=0))
+
--- train.py
+++ train.py
@@ -3,6 +3,17 @@ import pickle
 import numpy
 import os
 import cv2
+import csv
+import time
+from datetime import datetime
+
+#for speaking 
+
+from win32com.client import Dispatch
+
+def speak(str1):
+    speak = Dispatch(("SAPI.spVoice"))
+    speak.speak(str1)
 
 output = cv2.VideoCapture(0)
 faces = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
@@ -19,6 +30,13 @@ knn = KNeighborsClassifier(n_neighbors=5)
 
 knn.fit(FACES,LABLES)
 
+bg = cv2.imread('data/image.jpg')
+
+
+#working with csv file
+
+colNames = ['Name','Date','Time']
+
 while True:
     ok, frame = output.read()
     grayscalimg = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -34,14 +52,39 @@ while True:
 
         resize = cv2.resize(cropped, (40, 40)).flatten().reshape(1,-1)
         oput =  knn.predict(resize)
+        timeobj = time.time()
+        date = datetime.fromtimestamp(timeobj).strftime('%d-%m-%Y')
+        timestmp = datetime.fromtimestamp(timeobj).strftime('%H:%M:%S')
+
+        #check for the presence of file
+        present = os.path.isfile("attendence/attendence_ +" + str(date) + '.csv')    
+
 
         cv2.putText(frame,str(oput[0]),(x,y-15),cv2.FONT_HERSHEY_SCRIPT_COMPLEX,2,(0,0,0),2)
-
-    cv2.imshow("window", frame)
-
+        attendence = [str(oput[0]),str(date),str(timestmp)]
+    bg[162:162 + 480,455:455 + 640] = frame
+    cv2.imshow("window",bg)
     keybinding = cv2.waitKey(1)  # infinite time
 
+    if keybinding == ord('t'):
+        speak("Attendence marked successfully")
+        if present:
+            with open("attendence/attendence_ +" + str(date) + '.csv', "+a") as csvfile:
+                writer = csv.writer(csvfile)
+                
+                writer.writerow(attendence)
+            csvfile.close()
+            break
+        else:
+            with open("attendence/attendence_ +" + str(date) +'.csv', "+a") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(colNames)
+                writer.writerow(attendence)
+            csvfile.close()
+            break
+
     if keybinding == ord("k"):
+        speak("sayonara")
         break
 output.release()
 cv2.destroyAllWindows()
